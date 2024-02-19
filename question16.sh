0d2b364d1d64f1a6b47194d960bf6ce226a55b4d
DieuLinh0811 <122558373+DieuLinh0811@users.noreply.github.com>
2024-02-19 12:28:49 +1030

    solve

--- question16.sh
+++ question16.sh
@@ -1,6 +1,6 @@
-cd git-practice-02
+git fetch --all
 git checkout branch2
 git merge branch3
-git add .
-git commit -m "Resolved merge conflicts between branch3 and branch2"
+# git add .
+# git commit -m "Resolved merge conflicts between branch3 and branch2"
 git branch -d branch3
