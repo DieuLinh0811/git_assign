87830d76df81c4ce11ea43f18770993c175787dd
linhnda11 <26825773-linhnda11@users.noreply.replit.com>
2024-02-19 01:14:36 +0000

    all

---
+++ question11.sh
@@ -0,0 +1,7 @@
+git stash
+git checkout -b branch2
+touch file4
+git add file4
+git commit -m "Add file4"
+echo "Blehh" > file4
+git checkout main
---
+++ question13.sh
@@ -0,0 +1,6 @@
+git checkout -b submissionbranch
+git rm *.sh
+touch file13.txt
+git add file13.txt
+git commit -m file13.txt
+git push
